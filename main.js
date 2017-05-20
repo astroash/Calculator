477fe6f6f262f59f2fd5384d4a75323b9e581e5d
astroash <astroash@gmail.com>
2017-05-20 20:23:19 +0100

    individual button config

---
+++ main.js
@@ -0,0 +1,24 @@
+var btnAC = document.getElementById('btnAC');
+var btnCE = document.getElementById('btnCE');
+var btnDiv = document.getElementById('btnDiv');
+var btnMul = document.getElementById('btnMul');
+var btnAdd = document.getElementById('btnAdd');
+var btnSub = document.getElementById('btnSub');
+var btnDec = document.getElementById('btnDec');
+var btn0 = document.getElementById('btn0');
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+var btn4 = document.getElementById('btn4');
+var btn5 = document.getElementById('btn5');
+var btn6 = document.getElementById('btn6');
+var btn7 = document.getElementById('btn7');
+var btn8 = document.getElementById('btn8');
+var btn9 = document.getElementById('btn9');
+
+btnAC.onclick = function(){
+  console.log("AC")
+};
+btnCE.onclick = function(){
+  console.log(this.innerText)
+};
