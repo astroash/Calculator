0b389d42f77c6cbb9b10557502736b02220441b1
astroash <astroash@gmail.com>
2017-06-11 19:04:02 +0100

    Fuction for Buttons

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-var btnAC = document.getElementById('btnAC');
-var btnCE = document.getElementById('btnCE');
-var btnDiv = document.getElementById('btnDiv');
-var btnMul = document.getElementById('btnMul');
-var btnAdd = document.getElementById('btnAdd');
-var btnSub = document.getElementById('btnSub');
-var btnDec = document.getElementById('btnDec');
-var btn0 = document.getElementById('btn0');
-var btn1 = document.getElementById('btn1');
-var btn2 = document.getElementById('btn2');
-var btn3 = document.getElementById('btn3');
-var btn4 = document.getElementById('btn4');
-var btn5 = document.getElementById('btn5');
-var btn6 = document.getElementById('btn6');
-var btn7 = document.getElementById('btn7');
-var btn8 = document.getElementById('btn8');
-var btn9 = document.getElementById('btn9');
+function toNum() {
+  var temp =[];
+  for (i=0;i<arr.length;i++){
+    if (!isNaN(arr[i])){
+      temp.push(arr[i])
+    } else {
+      break;
+    }
+  }
+  return temp.join("")
+};
+// array to hold calulator input
+var arr = [];
 
-btnAC.onclick = function(){
-  console.log("AC")
-};
-btnCE.onclick = function(){
-  console.log(this.innerText)
-};
+// turns all number buttons to js elements
+var number = document.getElementsByClassName('number')
+for (var i=0;i<number.length;i++){
+  number[i].onclick = function(){
+    console.log(this.innerText);
+    arr.push(this.innerText);
+  };
+}
+// turns all operator buttons to js elements
+var operator = document.getElementsByClassName('operator')
+for (var i=0;i<operator.length;i++){
+  operator[i].onclick = function(){
+    console.log(this.innerText);
+    arr.push(this.innerText);
+  };
+}
+// Code for equals
+var equals = document.getElementById('btnEqu');
+var display = document.getElementById('display');
+equals.onclick = function(){
+  console.log(toNum());
+  display.innerText = toNum();
+}
